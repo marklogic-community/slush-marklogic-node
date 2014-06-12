31df4dc2fd4f532d451cc89db339ffe1391ea09b
Ryan Semerau <ryan.semerau@marklogic.com>
2014-06-12 15:42:31 -0400

    fixed problem with wrong excludes

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -24,14 +24,14 @@ var options = {
 };
 
 gulp.task('jshint', function() {
-  gulp.src('ui/app/**/*.js')
+  gulp.src('ui/app/**/*.js', '!ui/app/bower_components/**/*.js', '!ui/app/scripts/vendor/**/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Less
 gulp.task('less', function() {
-  return gulp.src(['ui/app/**/*.js', '!ui/app/bower_components/**/*.js', '!ui/app/scripts/vendor/**/*.js'])
+  return gulp.src(['ui/app/styles/*.less')
     .pipe(less())
     .pipe(gulp.dest('ui/app/styles/'));
 });
