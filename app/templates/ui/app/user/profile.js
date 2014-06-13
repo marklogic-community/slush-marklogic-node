4549105d5efecd94fcbcdb1f5c50f03159ae1739
Dave Cassel <dmcassel@gmail.com>
2014-06-13 11:31:02 -0400

    bug fix related to adding an email address to a profile

--- app/templates/ui/app/user/profile.js
+++ app/templates/ui/app/user/profile.js
@@ -1,16 +1,24 @@
 (function () {
   'use strict';
 
-  angular.module('sample')
+  angular.module('sample.user')
     .controller('ProfileCtrl', ['$scope', 'MLRest', 'User', '$location', function ($scope, mlRest, user, $location) {
       var model = {
-        user: user // GJo: a bit blunt way to insert the User service, but seems to work
+        user: user, // GJo: a bit blunt way to insert the User service, but seems to work
+        newEmail: ''
       };
 
       angular.extend($scope, {
         model: model,
         addEmail: function() {
-          $scope.model.user.emails.push('');
+          if ($scope.profileForm.newEmail.$error.email) {
+            return;
+          }
+          if (!$scope.model.user.emails) {
+            $scope.model.user.emails = [];
+          }
+          $scope.model.user.emails.push(model.newEmail);
+          model.newEmail = '';
         },
         removeEmail: function(index) {
           $scope.model.user.emails.splice(index, 1);
