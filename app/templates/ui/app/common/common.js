fc556778ae2c22f791beb59435e68b04eff1fecf
Ryan Semerau <ryan.semerau@marklogic.com>
2014-06-19 11:30:46 -0400

    added object2array and use that to preserve facet order from search options xml

--- app/templates/ui/app/common/common.js
+++ app/templates/ui/app/common/common.js
@@ -1,2 +1,13 @@
 
-angular.module('sample.common', []);
+var module = angular.module('sample.common', []);
+
+module.filter('object2Array', function() {
+  return function(input) {
+    var out = [];
+    for (var name in input) {
+    	input[name].__key = name;
+      out.push(input[name]);
+    }
+    return out;
+  };
+});
