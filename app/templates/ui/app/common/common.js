85c3733b1b7a43e4d14b9928198b210772855e4f
Geert Josten <geert.josten@marklogic.com>
2014-08-19 07:42:51 +0200

    Fixed #33: added use strict line

--- app/templates/ui/app/common/common.js
+++ app/templates/ui/app/common/common.js
@@ -1,13 +1,12 @@
 
-var module = angular.module('sample.common', []);
-
-module.filter('object2Array', function() {
+angular.module('sample.common', [])
+.filter('object2Array', function() {
   'use strict';
 
   return function(input) {
     var out = [];
     for (var name in input) {
-    	input[name].__key = name;
+      input[name].__key = name;
       out.push(input[name]);
     }
     return out;
