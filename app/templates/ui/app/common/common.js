1393b0a24d5a636d446a4a216b9fa7c1fc50ebe5
Dave Cassel <dmcassel@gmail.com>
2014-08-22 12:40:34 -0400

    formatting

--- app/templates/ui/app/common/common.js
+++ app/templates/ui/app/common/common.js
@@ -1,14 +1,14 @@
 
 angular.module('sample.common', [])
-.filter('object2Array', function() {
-  'use strict';
-  
-  return function(input) {
-    var out = [];
-    for (var name in input) {
-      input[name].__key = name;
-      out.push(input[name]);
-    }
-    return out;
-  };
+  .filter('object2Array', function() {
+    'use strict';
+
+    return function(input) {
+      var out = [];
+      for (var name in input) {
+        input[name].__key = name;
+        out.push(input[name]);
+      }
+      return out;
+    };
 });
