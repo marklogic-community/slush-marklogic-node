bc65ae3abff29179b5a45a08946ca5428a45d4bf
Bruce Holt <bruce.holt@marklogic.com>
2015-08-31 08:16:57 -0600

    Providing scripts/docs to run app as service

--- app/templates/ui/app/detail/similar-directive.js
+++ app/templates/ui/app/detail/similar-directive.js
@@ -13,13 +13,20 @@
       templateUrl: 'app/detail/similar-directive.html',
       scope: { uri: '@', limit: '@' },
       link: function(scope, iElement, iAttrs, ctrl) {
-        mlRest.extension('extsimilar', { method: 'GET', params: { 'rs:uri': scope.uri, 'rs:limit': scope.limit ? scope.limit : 10 }}).then(function(response) {
-          scope.similar = response.data.similar;
-        });
+        mlRest.extension('extsimilar',
+          {
+            method: 'GET',
+            params:
+              {
+                'rs:uri': scope.uri,
+                'rs:limit': scope.limit ? scope.limit : 10
+              }
+          })
+          .then(function(response) {
+            scope.similar = response.data.similar;
+          });
       }
     };
-
-
   }
 
 }());
