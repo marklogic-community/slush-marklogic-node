a0a081650d2ebbd85d5e78cf02c46ab50d6a051b
Patrick McElwee <pmcelwee@gmail.com>
2015-09-03 15:53:12 -0400

    Do not ask for appname twice
    Fixes #133
    
    Also, make it optional on the command line

--- slushfile.js
+++ slushfile.js
@@ -78,20 +78,16 @@ function processInput() {
     } else {
       inputs.appName = arg;
     }
-  })
-  if (inputs.hasOwnProperty('appName')) {
-    return inputs;
-  } else {
-    throw 'You must supply a name for your application. For example: slush marklogic-node <your-name>';
-  }
+  });
+  return inputs;
 }
 
-function getNameProposal (fallback) {
+function getNameProposal() {
   var path = require('path');
   try {
     return require(path.join(process.cwd(), 'package.json')).name;
   } catch (e) {
-    return fallback;
+    return path.basename(process.cwd());
   }
 }
 
@@ -291,16 +287,25 @@ gulp.task('init', ['checkForUpdates'], function (done) {
   var appType = clArgs.appType;
   var branch =  clArgs.branch;
 
-  inquirer.prompt([
-    {type: 'input', name: 'name', message: 'Name for the app?', default: getNameProposal(appName)},
+  var prompts = [
     {type: 'input', name: 'nodePort', message: 'Node app port?', default: 9070},
     {type: 'input', name: 'appPort', message: 'MarkLogic App/Rest port?', default: 8040},
     {type: 'input', name: 'xccPort', message: 'XCC port?', default:8041},
     {type: 'list', name: 'mlVersion', message: 'MarkLogic version?', choices: ['8','7', '6', '5'], default: 0},
     {type: 'confirm', name: 'includeEsri', message: 'Include ESRI Maps?', default: false}
-  ],
-  function (answers) {
-    answers.nameDashed = _.slugify(answers.name);
+  ];
+
+  if (typeof appName === 'undefined') {
+    prompts.unshift(
+      {type: 'input', name: 'name', message: 'Name for the app?', default: getNameProposal()});
+  }
+
+  inquirer.prompt(prompts, function (answers) {
+    if (typeof appName === 'undefined') {
+      answers.nameDashed = _.slugify(answers.name);
+    } else {
+      answers.nameDashed = _.slugify(appName);
+    }
     settings.nodePort = answers.nodePort;
     settings.appPort = answers.appPort;
     settings.includeEsri = answers.includeEsri;
