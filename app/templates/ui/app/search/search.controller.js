3108d471a6a23ba9c6b8240f695929da94de7d5b
Hunter Williams <hunter@huntergwilliams.com>
2015-09-15 10:02:37 +0700

    Copied @grtjn changes. Using as starting point

--- app/templates/ui/app/search/search.controller.js
+++ app/templates/ui/app/search/search.controller.js
@@ -3,36 +3,56 @@
   'use strict';
 
   angular.module('app.search')
+    .factory('SearchModel', SearchModel)
     .controller('SearchCtrl', SearchCtrl);
 
-  SearchCtrl.$inject = ['$scope', '$location', 'userService', 'searchContextService',
-   'MLSearchFactory'];
+  SearchModel.$inject = ['MLSearchFactory'];
+  SearchCtrl.$inject = ['$scope', '$location', 'userService', 'SearchModel'];
 
   // inherit from MLSearchController
   var superCtrl = MLSearchController.prototype;
   SearchCtrl.prototype = Object.create(superCtrl);
 
-  function SearchCtrl($scope, $location, userService, searchContextService, searchFactory) {
+  function SearchModel(searchFactory) {
+    var mlSearch = searchFactory.newContext();
+    return {
+      mlSearch: mlSearch,
+      response: null
+    };
+  }
+
+  function SearchCtrl($scope, $location, userService, searchModel) {
     var ctrl = this;
-    var initialRun = false;
-
-    if (!searchContextService.currentSearchContext()) {
-      initialRun = true;
-      searchContextService.updateSearchContext(searchFactory.newContext());
-    }
-
-    var mlSearch = searchContextService.currentSearchContext();
+    var mlSearch = searchModel.mlSearch;
 
     superCtrl.constructor.call(ctrl, $scope, $location, mlSearch);
 
-    if (initialRun) {
-      ctrl.init();
-    }
-    else
-    {
-      ctrl.updateURLParams();
-      ctrl.search();
-    }
+    //ctrl.init();
+    (function init() {
+      // monitor URL params changes (forward/back, etc.)
+      ctrl.$scope.$on('$locationChangeSuccess', ctrl.locationChange.bind(ctrl));
+
+      // capture initial URL params in mlSearch and ctrl
+      if ( ctrl.parseExtraURLParams ) {
+        ctrl.parseExtraURLParams();
+      }
+
+      if (searchModel.response) {
+        ctrl.response = searchModel.response;
+        ctrl.updateURLParams();
+      } else {
+        ctrl.mlSearch.fromParams().then( ctrl._search.bind(ctrl) );
+      }
+    })();
+
+    ctrl.updateSearchResults = function updateSearchResults(data) {
+      searchModel.response = data;
+      this.searchPending = false;
+      this.response = data;
+      this.qtext = this.mlSearch.getText();
+      this.page = this.mlSearch.getPage();
+      return this;
+    };
 
     ctrl.setSnippet = function(type) {
       mlSearch.setSnippet(type);
