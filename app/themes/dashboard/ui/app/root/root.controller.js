cea9d903362bad1289ce2e09cb2c7f497e42cebd
Matt Pileggi <Matt.Pileggi@marklogic.com>
2016-03-17 14:16:23 -0400

    initial commit of starter files

---
+++ app/themes/dashboard/ui/app/root/root.controller.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  angular.module('app.root')
+    .controller('RootCtrl', RootCtrl);
+
+  RootCtrl.$inject = ['messageBoardService'];
+
+  function RootCtrl(messageBoardService) {
+    var ctrl = this;
+    angular.extend(ctrl, {
+      messageBoardService: messageBoardService,
+      currentYear: new Date().getUTCFullYear()
+    });
+  }
+}());
