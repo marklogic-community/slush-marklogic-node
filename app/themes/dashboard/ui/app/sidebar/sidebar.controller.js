c87fcc3fe70fa9134f2ec1a617cf7b1ede4060e6
jan michael yu <jan.michael.yu+github@gmail.com>
2016-06-01 18:41:52 +0800

    changing sideboard implementation to scope watch

---
+++ app/themes/dashboard/ui/app/sidebar/sidebar.controller.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  angular.module('app').controller('SidebarController', SidebarController);
+
+  SidebarController.$inject = ['$scope', 'navService'];
+  function SidebarController ($scope, navService) {
+    var ctrl = this;
+    ctrl.navService = navService;
+    $scope.$watch(ctrl.navService.getState, function (newVal) {
+       $scope.sidebarState = newVal;
+    });
+  }
+}());
