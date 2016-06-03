5af0d7cc4c2cb5bd19e453460a5d335c99e0227a
Jan Michael Yu <Jan.MichaelYu@marklogic.com>
2016-06-03 19:10:09 +0800

    using ngClass and moving sidebar controller to inner div

--- app/themes/dashboard/ui/app/sidebar/sidebar.controller.js
+++ app/themes/dashboard/ui/app/sidebar/sidebar.controller.js
@@ -1,14 +1,22 @@
-(function () {
+(function() {
   'use strict';
 
   angular.module('app').controller('SidebarController', SidebarController);
 
   SidebarController.$inject = ['$scope', 'navService'];
-  function SidebarController ($scope, navService) {
+
+  function SidebarController($scope, navService) {
     var ctrl = this;
     ctrl.navService = navService;
-    $scope.$watch(ctrl.navService.getState, function (newVal) {
-       $scope.sidebarState = newVal;
+    $scope.$watch(ctrl.navService.getState, function(newVal) {
+      $scope.sidebarState = newVal;
+      if (newVal) {
+        $scope.sidebarClass = 'wrapper skin-black sidebar-mini sidebar-open';
+      } else {
+        $scope.sidebarClass = 'wrapper skin-black sidebar-mini sidebar-collapse';
+      }
+
     });
+
   }
 }());
