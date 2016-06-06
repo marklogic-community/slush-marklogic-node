d91242d57053d2f9c8656ca5c2771a7fb36a1de3
Geert Josten <geert.josten@marklogic.com>
2016-06-06 20:54:30 +0200

    Final brush through map theme, added drawing of rectangles..

--- app/themes/map/ui/app/map/map-manager.service.js
+++ app/themes/map/ui/app/map/map-manager.service.js
@@ -1,11 +1,13 @@
 (function () {
   'use strict';
 
-  var app = angular.module('app');
+  angular.module('app.root')
+    .service('MLUiGmapManager', ['$rootScope', '$timeout', 'uiGmapGoogleMapApi', MapManager]);
 
-  app.service('MLUiGmapManager', ['$rootScope', '$timeout', MapManager]);
+  function MapManager($rootScope, $timeout, $googleMapsApi) {
+    // Google Maps API is loaded asynchroniously..
+    var $googleMaps = null;
 
-  function MapManager($rootScope, $timeout) {
     // internal defaults
     var defaultMapOptions = {
       scrollwheel: true,
@@ -13,6 +15,7 @@
       disableDefaultUI: true,
       zoomControl: true
     };
+    var defaultDrawingOptions = null;
     var defaultCenter = { latitude: 52.0325133, longitude: 5.2289087 };
     var defaultZoom = 2;
     var defaultBounds = {
@@ -28,6 +31,10 @@
     var defaultEvents = {
       rightclick: function() {
         $timeout(function () {
+          angular.forEach(service.drawings, function(drawing) {
+            drawing.setMap(null);
+          });
+          service.drawings = [];
           resetMap();
         },0);
       }
@@ -39,7 +46,7 @@
       'grey',
       'purple'
     ];
-    var resettingMap = true;
+    var changingBounds = true;
     var mlBounds = null;
 
     // keep track of multiple sets of markers
@@ -50,22 +57,15 @@
 
     // service interface
     var service = {
-      // properties
-      mapOptions: defaultMapOptions,
-      center: angular.copy(defaultCenter),
-      zoom: angular.copy(defaultZoom),
-      bounds: angular.copy(defaultBounds),
-      events: defaultEvents,
-      markerMode: 'results',
-      colors: angular.copy(defaultColors),
-      
       // method
       init: init,
+      restoreDefaults: restoreDefaults,
       getColor: getColor,
       getMarkers: getMarkers,
       setMarkers: setMarkers,
       setResultMarkers: setResultMarkers,
       setFacetMarkers: setFacetMarkers,
+      setBounds: setBounds,
       watchBounds: watchBounds,
       resetMap: resetMap
     };
@@ -75,6 +75,23 @@
       defaultCenter = newCenter;
       defaultZoom = newZoom;
     }
+
+    // (re)set properties to defaults
+    function restoreDefaults() {
+      angular.extend(service, {
+        mapOptions: angular.copy(defaultMapOptions),
+        drawingOptions: defaultDrawingOptions ? angular.copy(defaultDrawingOptions) : null,
+        center: angular.copy(defaultCenter),
+        zoom: angular.copy(defaultZoom),
+        bounds: angular.copy(defaultBounds),
+        events: defaultEvents,
+        drawings: [],
+        drawingControl: {},
+        markerMode: 'facets',
+        colors: angular.copy(defaultColors),
+      });
+    }
+    restoreDefaults();
 
     // the only property access wrappers
     function getColor(colorId) {
@@ -140,17 +157,41 @@
       setMarkers('facets', newMarkers);
     }
 
+    // to consume Google Maps bounds directly
+    function setBounds(googleBounds) {
+      if (googleBounds && googleBounds.getSouthWest) {
+        $timeout(function () {
+          service.bounds = {
+            southwest: {
+              latitude: googleBounds.getSouthWest().lat(),
+              longitude: googleBounds.getSouthWest().lng()
+            },
+            northeast: {
+              latitude: googleBounds.getNorthEast().lat(),
+              longitude: googleBounds.getNorthEast().lng()
+            }
+          };
+        });
+      }
+    }
+
     $rootScope.$watch(function() { return service.bounds; }, function() {
       // prepare bounds for consumption by ML search
-      if (!resettingMap && service.bounds && service.bounds.southwest && service.bounds.southwest.latitude) {
-        mlBounds = {
-          'south': service.bounds.southwest.latitude,
-          'west': service.bounds.southwest.longitude,
-          'north': service.bounds.northeast.latitude,
-          'east': service.bounds.northeast.longitude
-        };
-      } else {
-        mlBounds = null;
+      if (!changingBounds) {
+        if (service.bounds && service.bounds.southwest && service.bounds.southwest.latitude) {
+          changingBounds = true;
+          mlBounds = {
+            'south': service.bounds.southwest.latitude,
+            'west': service.bounds.southwest.longitude,
+            'north': service.bounds.northeast.latitude,
+            'east': service.bounds.northeast.longitude
+          };
+          $timeout(function() {
+            changingBounds = false;
+          }, 2000);
+        } else {
+          mlBounds = null;
+        }
       }
     }, true);
 
@@ -159,17 +200,43 @@
     }
 
     function resetMap() {
-      resettingMap = true;
+      changingBounds = true;
       service.center = angular.copy(defaultCenter);
       service.zoom = angular.copy(defaultZoom);
-      //service.bounds = angular.copy(defaultBounds);
+      mlBounds = null;
       $timeout(function() {
-        resettingMap = false;
+        changingBounds = false;
       }, 2000);
     }
 
+    // Delayed init of drawingOptions, waits for Google Maps API..
+    $googleMapsApi.then(function($g) {
+      $googleMaps = $g;
+      if ($googleMaps.drawing) {
+        defaultDrawingOptions = {
+          drawingControl: true,
+          drawingControlOptions: {
+            position: $googleMaps.ControlPosition.TOP_CENTER,
+            drawingModes: [
+              $googleMaps.drawing.OverlayType.RECTANGLE
+            ]
+          }
+        };
+        service.drawingOptions = service.drawingOptions || angular.copy(defaultDrawingOptions);
+      }
+    });
+
+    $rootScope.$watch(function() { return service.drawingControl.getDrawingManager; }, function(manager) {
+      if ($googleMaps && manager) {
+        $googleMaps.event.addListener(manager(), 'overlaycomplete', function(overlayEvent) {
+          service.drawings.push(overlayEvent.overlay);
+          setBounds(overlayEvent.overlay.getBounds());
+        });
+      }
+    });
+
     $timeout(function() {
-      resettingMap = false;
+      changingBounds = false;
     }, 2000);
 
     return service;
--- app/themes/map/ui/app/root/root.controller.js
+++ app/themes/map/ui/app/root/root.controller.js
@@ -38,10 +38,10 @@
   }
 
   RootCtrl.$inject = ['messageBoardService', 'userService', '$scope',
-    '$rootScope', '$templateRequest', '$compile', 'rootUtils', 'MLUiGmapManager'];
+    '$rootScope', '$templateRequest', '$compile', 'rootUtils', 'MLUiGmapManager', 'uiGmapGoogleMapApi'];
 
   function RootCtrl(messageBoardService, userService, $scope,
-    $rootScope, $templateRequest, $compile, rootUtils, mlMapManager) {
+    $rootScope, $templateRequest, $compile, rootUtils, mlMapManager, $googleMapsApi) {
 
     var ctrl = this;
     ctrl.currentYear = new Date().getUTCFullYear();
@@ -54,6 +54,11 @@
     var miw = window.jQuery('#app-mobile-info-window').get(0); // FIXME: use angular.element?
     var miwscope = $rootScope.$new(), mobileWin;
     var pixelOffset,shownMarker;
+    var $googleMaps = null;
+
+    $googleMapsApi.then(function($gMaps) {
+      $googleMaps = $gMaps;
+    });
 
     ctrl.mapManager = mlMapManager;
 
@@ -72,20 +77,24 @@
     // FIXME: can we make more use of ui-gmap-window nested inside ui-gmap-markers directive?
     //        Alternatively, push away part of this code into a service. RootUtils perhaps?
     ctrl.markerClick = function(inst,evt,marker) {
+      if (!$googleMaps) {
+        return;
+      }
+
       if (!pixelOffset) {
-        pixelOffset = new google.maps.Size(0, -30);
+        pixelOffset = new $googleMaps.Size(0, -30);
         ctrl.infoWindow.options = { pixelOffset: pixelOffset };
       }
 
       var lat = inst.getPosition().lat() + 20;
       var lng = inst.getPosition().lng();
-      var position = new google.maps.LatLng(lat, lng, true);
+      var position = new $googleMaps.LatLng(lat, lng, true);
 
       if (! marker.content) {
         inst.map.setCenter(position);
       } else if (rootUtils.isMobile()) {
         if (!mobileWin) {
-          mobileWin = new google.maps.InfoWindow({ content: '<span>' + marker.title + '</span>' });
+          mobileWin = new $googleMaps.InfoWindow({ content: '<span>' + marker.title + '</span>' });
           google.maps.event.addListener(mobileWin, 'closeclick', function() {
             miwscope.parameter.showMe = false;
             shownMarker = null;
