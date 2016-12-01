3ece7838519ea8d473caba01d9b107eca3766265
Geert Josten <geert.josten@marklogic.com>
2016-12-01 20:42:12 +0100

    Test scripts

--- test-generate-all.sh
+++ test-generate-all.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-## SLUSH DEFAULT ##
-
-if [ -d slush-default ]; then
-  cd slush-default
-  ./ml local wipe
-  ./ml local restart
-  cd ..
-  rm -rf slush-default
-fi
-
-gulp --gulpfile=slushfile.js --app-name=slush-default --theme=default --ml-version=8 --ml-host=ml8-ml1 --ml-admin-user=admin --ml-admin-pass=admin ---ml-http-port=8040 --node-port=8050 --guest-access=true --disallow-updates=true --appusers-only=true
-
-cd slush-default
-
-gulp build
-
-./ml local bootstrap local deploy modules local deploy content local mlcp -options_file import-sample-data.options
-
-cd ..
-
 ## SLUSH CARDS ##
 
 if [ -d slush-cards ]; then
@@ -41,6 +21,26 @@ gulp build
 cd ..
 
 exit
+
+## SLUSH DEFAULT ##
+
+if [ -d slush-default ]; then
+  cd slush-default
+  ./ml local wipe
+  ./ml local restart
+  cd ..
+  rm -rf slush-default
+fi
+
+gulp --gulpfile=slushfile.js --app-name=slush-default --theme=default --ml-version=8 --ml-host=ml8-ml1 --ml-admin-user=admin --ml-admin-pass=admin ---ml-http-port=8040 --node-port=8050 --guest-access=true --disallow-updates=true --appusers-only=true
+
+cd slush-default
+
+gulp build
+
+./ml local bootstrap local deploy modules local deploy content local mlcp -options_file import-sample-data.options
+
+cd ..
 
 ## SLUSH 3COLUMN ##
 
