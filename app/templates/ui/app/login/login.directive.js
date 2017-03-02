b86738c19f7dda76cc1d3015774626cb450640a0
Geert Josten <geert.josten@marklogic.com>
2017-03-02 09:17:48 +0100

    Fixed  in directives

--- app/templates/ui/app/login/login.directive.js
+++ app/templates/ui/app/login/login.directive.js
@@ -9,7 +9,7 @@
     return {
       restrict: 'EA',
       controller: 'LoginCtrl',
-      controllerAs: 'ctrl',
+      controllerAs: '$ctrl',
       replace: true,
       scope: {
         showCancel: '=',
